f2325944e565ffd26c4aaa3a8b8b782dff9d716c
jakubesh <jwiacek@egnyte.com>
2024-05-23 20:20:26 +0200

    Added next.js and github pages workflow

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    basePath: "/fizjoterapia-kasia-jankowska-wiacek"
+};
+
+export default nextConfig;
